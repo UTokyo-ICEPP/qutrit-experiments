sign of zval was wrong in ef_discriminator analysis

qutrit_experiments/configurations/single_qutrit.py
@@ -1,6 +1,7 @@
 # pylint: disable=import-outside-toplevel, function-redefined, unused-argument
 """Single qutrit calibration and characterization experiments."""
 import numpy as np
+from qiskit_experiments.visualization import MplDrawer, IQPlotter
 
 from .qutrit import (qutrit_rough_frequency, qutrit_rough_frequency_post, qutrit_rough_amplitude,
                      qutrit_semifine_frequency, qutrit_fine_frequency, qutrit_rough_x_drag,
@@ -54,5 +55,14 @@ def qutrit_rough_amplitude(runner, experiment_data):
 
     amps = np.array([d['metadata']['xval'] for d in experiment_data.data()])
     theta, dist = ef_discriminator_analysis(experiment_data, np.argmin(np.abs(amps)))
-    runner.program_data['iq_discriminator'] = {runner.program_data['qubit']:
-                                               LinearDiscriminator(theta, dist)}
+    discriminator = LinearDiscriminator(theta, dist)
+    runner.program_data['iq_discriminator'] = {runner.program_data['qubit']: discriminator}
+
+    if False:
+        for iamp, datum in enumerate(experiment_data.data()):
+            amplitude = datum['metadata']['amplitude']
+            plotter = IQPlotter(MplDrawer())
+            plotter.set_series_data('0', points=np.squeeze(datum['memory']))
+            plotter.set_figure_options(series_params={'0': {'label': f'amp={amplitude}'}})
+            plotter.set_supplementary_data(discriminator=discriminator)
+            experiment_data.add_figures(plotter.figure(), f'iq_{iamp}')

qutrit_experiments/experiments/delay_phase_offset.py
@@ -20,7 +20,6 @@ from ..constants import DEFAULT_SHOTS
 from ..experiment_mixins.ef_space import EFSpaceExperiment
 from ..experiment_mixins.map_to_physical_qubits import MapToPhysicalQubits
 from ..gates import RZ12Gate, SX12Gate
-from ..transpilation import map_to_physical_qubits
 from ..util.dummy_data import ef_memory, single_qubit_counts
 
 twopi = 2. * np.pi
@@ -286,8 +285,8 @@ class RamseyPhaseSweepAnalysis(curve.CurveAnalysis):
             outcome='1',
             data_subfit_map={f'delay{delay}': {'delay': delay} for delay in delay_durations},
             bounds={
-                'amp': (0.4, 0.6),
-                'base': (0.4, 0.6),
+                'amp': (0., 0.6),
+                'base': (0., 1.),
                 'epsilon': (-np.pi, np.pi)
             }
         )

qutrit_experiments/util/ef_discriminator.py
@@ -65,6 +65,7 @@ from qiskit_experiments.data_processing import DataProcessor
 if TYPE_CHECKING:
     from qiskit_experiments.framework import ExperimentData
 
+
 def fit_discriminator_boundary(
     wval: np.ndarray,
     samples: np.ndarray,
@@ -114,6 +115,11 @@ def fit_discriminator_boundary(
     if np.cos(2. * np.pi * freq * wval[idx_i1] + phase) < 0.:
         phase += np.pi
 
+    # Scale the samples and dist down
+    samples /= centroids_dist
+    dist /= centroids_dist
+
+    # Constant coefficients
     cval = np.cos(2. * np.pi * freq * wval + phase)
     sumc = np.sum(cval)
     cmat_inv_row0 = np.linalg.inv([[sumc, wval.shape[0]],
@@ -121,17 +127,17 @@ def fit_discriminator_boundary(
 
     def minus_ahat(params):
         nvec = jnp.array([jnp.cos(params[0]), jnp.sin(params[0])])
-        zval = jnp.mean(jnp.tanh((jnp.dot(samples, nvec) - params[1]) / centroids_dist), axis=1)
+        zval = -jnp.mean(jnp.tanh(jnp.dot(samples, nvec) - params[1]), axis=1)
         return -jnp.dot(cmat_inv_row0, jnp.array([jnp.sum(zval), jnp.sum(cval * zval)]))
 
-    solver = jaxopt.BFGS(fun=minus_ahat, maxiter=maxiter, tol=convergence)
+    solver = jaxopt.GradientDescent(fun=minus_ahat, maxiter=maxiter, tol=convergence)
     res = solver.run(jnp.array([theta, dist]))
     theta, dist = map(float, res.params)
-
     while theta > np.pi:
         theta -= 2. * np.pi
     while theta < -np.pi:
         theta += 2. * np.pi
+    dist *= centroids_dist
 
     return theta, dist
 