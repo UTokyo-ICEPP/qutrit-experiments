7cca21868f7e6566a808a271bb6301a28ba6b3e0
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-02-08 06:02:16 +0900

    Finally a working version

--- qutrit_experiments/experiments/qutrit_qubit_cx/cr_fine_width.py
+++ qutrit_experiments/experiments/qutrit_qubit_cx/cr_fine_width.py
@@ -1,7 +1,12 @@
 from collections.abc import Sequence
 from typing import Optional
+from matplotlib.figure import Figure
+import jax
+import jax.numpy as jnp
+import jaxopt
 import numpy as np
 import scipy.optimize as sciopt
+from uncertainties import correlated_values, ufloat, unumpy as unp
 from qiskit import QuantumCircuit
 from qiskit.circuit import Parameter
 from qiskit.pulse import ScheduleBlock
@@ -11,7 +16,9 @@ from qiskit_experiments.calibration_management.update_library import BaseUpdater
 from qiskit_experiments.framework import AnalysisResultData, ExperimentData, Options
 from qiskit_experiments.visualization import CurvePlotter, MplDrawer
 
-from ..qutrit_qubit_tomography import QutritQubitTomographyScan, QutritQubitTomographyScanAnalysis
+from ...util.bloch import su2_cartesian, su2_cartesian_axnorm, su2_cartesian_params
+from ..qutrit_qubit.qutrit_qubit_tomography import (QutritQubitTomographyScan,
+                                                    QutritQubitTomographyScanAnalysis)
 from .util import RCRType, get_margin, make_crcr_circuit
 
 twopi = 2. * np.pi
@@ -35,6 +42,7 @@ class CycledRepeatedCRWidth(QutritQubitTomographyScan):
         width_param_name: str = 'width',
         margin_param_name: str = 'margin',
         widths: Optional[Sequence[float]] = None,
+        measure_preparations: bool = True,
         backend: Optional[Backend] = None
     ):
         if widths is None:
@@ -55,7 +63,8 @@ class CycledRepeatedCRWidth(QutritQubitTomographyScan):
 
         super().__init__(physical_qubits,
                          make_crcr_circuit(physical_qubits, cr_schedules, None, rcr_type),
-                         param_names, param_values, backend=backend)
+                         param_names, param_values, measure_preparations=measure_preparations,
+                         backend=backend)
         self.analysis = CycledRepeatedCRWidthAnalysis([exp.analysis for exp in self._experiments])
 
 
@@ -65,52 +74,151 @@ class CycledRepeatedCRWidthAnalysis(QutritQubitTomographyScanAnalysis):
         self,
         experiment_data: ExperimentData,
         analysis_results: list[AnalysisResultData],
-        figures: list["matplotlib.figure.Figure"]
-    ) -> tuple[list[AnalysisResultData], list["matplotlib.figure.Figure"]]:
+        figures: list[Figure]
+    ) -> tuple[list[AnalysisResultData], list[Figure]]:
+        """Fit exp[-i/2 * (slope * width + intercept) * axis.pauli] to the observed unitaries."""
         analysis_results, figures = super()._run_additional_analysis(experiment_data,
                                                                      analysis_results, figures)
 
-        def curve(w, a, b):
-            return (np.asarray(w) * a + b) % twopi
+        # Shape [num_widths, num_control_states(=3), 3]
+        unitary_params = next(res.value for res in analysis_results
+                              if res.name == 'unitary_parameters')
+        unitary_params_n = unp.nominal_values(unitary_params)
+        scan_idx = experiment_data.metadata['scan_parameters'].index('crp_width')
+        widths = np.array(experiment_data.metadata['scan_values'][scan_idx])
+        x_interp = np.linspace(widths[0], widths[-1], 100)
 
-        widths = next(res.value for res in analysis_results if res.name == 'crp_width')
-        unitaries = next(res.value for res in analysis_results if res.name == 'unitary_parameters')
-        xdiffs = (unitaries[:, 1, 0] - unitaries[:, 0, 0]) % twopi
-        xdiff_steps = np.diff(xdiffs)
-        imin = np.argmin(np.abs(xdiff_steps))
-        p0_a = xdiff_steps[imin] / np.diff(widths)[0]
-        p0_b = xdiffs[0] - p0_a * widths[0]
-        popt, _ = sciopt.curve_fit(curve, widths, xdiffs, p0=(p0_a, p0_b))
-        slope, intercept = popt
-        # Find the minimum w>0 that satisfies a*w + b = (2n+1)*π  (n ∈ Z)
+        # Fit in the unitary space
+        def axis_from_params(params, npmod=np):
+            psi, phi = params[2:]
+            return npmod.array([
+                npmod.sin(psi) * npmod.cos(phi),
+                npmod.sin(psi) * npmod.sin(phi),
+                npmod.cos(psi)
+            ])
+
+        def angle_from_params(params, widths):
+            slope, intercept = params[:2]
+            return slope * widths + intercept
+
+        def fidelities(params, ic, npmod=np):
+            test_u = su2_cartesian_axnorm(
+                axis_from_params(params, npmod=npmod),
+                angle_from_params(params, widths),
+                npmod=npmod
+            )
+            # Has to be a jnp array because ic is
+            udag = su2_cartesian(npmod.array(unitary_params_n)[:, ic], npmod=npmod).conjugate()
+            return npmod.square(npmod.abs(npmod.einsum('wij,wij->w', udag, test_u) / 2.))
+
+        @jax.jit
+        def objective(params, ic):
+            return 1. - jnp.mean(fidelities(params, ic, npmod=jnp))
+
+        # Negative log fidelity has too many local minima and is not a good objective for fitting,
+        # but the uncertainties should be evaluated through this quantity
+        # (There must be some mistake in my reasoning though; the uncertainties appear too large)
+        def negative_log_fidelity(params, ic):
+            return -jnp.sum(jnp.log(fidelities(params, ic, npmod=jnp)))
+
+        solver = jaxopt.GradientDescent(objective, maxiter=10000)
+
+        popt_ufloats = []
+
+        for ic in range(2):
+            axes = unitary_params_n[:, ic].copy()
+            axes /= np.sqrt(np.sum(np.square(axes), axis=-1))[:, None]
+
+            # Align along a single orientation and take the mean
+            mean_ax = np.mean(axes * np.where(axes @ axes[0] < 0., -1., 1.)[:, None], axis=0)
+            mean_ax /= np.sqrt(np.sum(np.square(mean_ax)))
+            psi = np.arccos(min(max(mean_ax[2], -1.), 1.))
+            phi = np.arctan2(mean_ax[1], mean_ax[0])
+
+            # Make a mesh of slope and intercept values as initial value candidates
+            slopes = np.linspace(0., twopi / (widths[-1] - widths[0]), 4)
+            intercepts = np.linspace(0., twopi, 4, endpoint=False)
+            islope, iint = np.ogrid[:len(slopes), :len(intercepts)]
+            p0s = np.empty((2, len(slopes), len(intercepts), 4))
+            p0s[..., 0] = slopes[islope]
+            p0s[..., 1] = intercepts[iint]
+            # Use both orientations
+            p0s[0, ..., 2:] = [psi, phi]
+            p0s[1, ..., 2:] = [np.pi - psi, np.pi + phi]
+
+            fit_result = jax.vmap(solver.run, in_axes=[0, None])(p0s.reshape(-1, 4), ic)
+            fvals = jax.vmap(objective, in_axes=[0, None])(fit_result.params, ic)
+            iopt = np.argmin(fvals)
+            popt = np.array(fit_result.params[iopt])
+            if popt[0] < 0.:
+                # Slope must be positive - invert the sign and the axis orientation
+                popt[0:2] *= -1.
+                popt[2] = np.pi - popt[2]
+                popt[3] += np.pi
+            # Intercept must be in [0, 2pi]
+            popt[1] %= twopi
+            # Keep phi in [-pi, pi]
+            popt[3] = (popt[3] + np.pi) % twopi - np.pi
+
+            pcov = np.linalg.inv(jax.hessian(negative_log_fidelity)(popt, ic))
+            popt_ufloats.append(correlated_values(nom_values=popt, covariance_mat=pcov,
+                                tags=['slope', 'intercept', 'psi', 'phi']))
+
+            if self.options.plot:
+                plotter = CurvePlotter(MplDrawer())
+                plotter.set_figure_options(
+                    xlabel='CR width',
+                    ylabel='angle',
+                    ylim=(-np.pi - 0.1, np.pi + 0.1)
+                )
+                axis = axis_from_params(popt)
+                # Constrain theta to [-pi, pi] because the observed unitary parameters are in that
+                # domain
+                theta = (angle_from_params(popt, x_interp) + np.pi) % twopi - np.pi
+                xyz_pred = su2_cartesian_params(su2_cartesian_axnorm(axis, theta))
+                for iax, ax in enumerate(['x', 'y', 'z']):
+                    plotter.set_series_data(
+                        ax,
+                        x_formatted=widths,
+                        y_formatted=unitary_params_n[:, ic, iax],
+                        y_formatted_err=unp.std_devs(unitary_params[:, ic, iax]),
+                        x_interp=x_interp,
+                        y_interp=xyz_pred[:, iax]
+                    )
+                figures.append(plotter.figure())
+
+        popt_ufloats = np.array(popt_ufloats)
+        analysis_results.append(
+            AnalysisResultData('unitary_line_fit_params', value=popt_ufloats)
+        )
+
+        # Slope and intercept (with uncertainties) of x[1] - x[0]
+        lineparam = popt_ufloats[:, :2]
+        psi, phi = popt_ufloats.T[2:]
+        slope, intercept = (lineparam[1] * unp.sin(psi[1]) * unp.cos(phi[1])
+                            - lineparam[0] * unp.sin(psi[0]) * unp.cos(phi[0]))
         wmin = (np.pi - intercept) / slope
-        while wmin < 0.:
-            wmin += twopi / np.abs(slope)
-        while (wtest := wmin - twopi / np.abs(slope)) > 0.:
+        while wmin.n < 0.:
+            wmin += twopi / np.abs(slope.n)
+        while (wtest := wmin - twopi / np.abs(slope.n)).n > 0.:
             wmin = wtest
-        rabi_phase_diff = slope * wmin + intercept
+        cx_sign = np.sign(slope.n * wmin.n + intercept.n)
 
         analysis_results.extend([
             AnalysisResultData(name='cr_width', value=wmin),
-            AnalysisResultData(name='cx_sign', value=np.sign(rabi_phase_diff))
+            AnalysisResultData(name='cx_sign', value=cx_sign)
         ])
 
         if self.options.plot:
             plotter = CurvePlotter(MplDrawer())
             plotter.set_figure_options(
                 xlabel='CR width',
-                ylabel=r'$\theta_1 - \theta_0$',
-                ylim=(-twopi - 0.1, twopi + 0.1)
+                ylabel=r'$\theta_1 - \theta_0$'
             )
-            x_interp = np.linspace(widths[0], widths[-1], 100)
-            y_interp = (curve(x_interp, slope, intercept) + np.pi) % twopi - np.pi
             plotter.set_series_data(
                 'angle_diff',
-                x_formatted=widths,
-                y_formatted=(unitaries[:, 1, 0] - unitaries[:, 0, 0] + twopi) % (2. * twopi) - twopi,
-                y_formatted_err=np.zeros_like(widths),
                 x_interp=x_interp,
-                y_interp=y_interp
+                y_interp=slope.n * x_interp + intercept.n
             )
             figures.append(plotter.figure())
 
@@ -127,6 +235,7 @@ class CycledRepeatedCRWidthCal(BaseCalibrationExperiment, CycledRepeatedCRWidth)
         cal_parameter_name: list[str] = ['width', 'margin'],
         schedule_name: str = 'cr',
         widths: Optional[Sequence[float]] = None,
+        measure_preparations: bool = True,
         auto_update: bool = True
     ):
         assign_params = {pname: Parameter(pname) for pname in cal_parameter_name}
@@ -149,14 +258,15 @@ class CycledRepeatedCRWidthCal(BaseCalibrationExperiment, CycledRepeatedCRWidth)
             auto_update=auto_update,
             width_param_name=cal_parameter_name[0],
             margin_param_name=cal_parameter_name[1],
-            widths=widths
+            widths=widths,
+            measure_preparations=measure_preparations
         )
 
     def _attach_calibrations(self, circuit: QuantumCircuit):
         pass
 
     def update_calibrations(self, experiment_data: ExperimentData):
-        width = experiment_data.analysis_results('cr_width', block=False).value
+        width = experiment_data.analysis_results('cr_width', block=False).value.n
         assign_params = {pname: Parameter(pname) for pname in self._param_name}
         sched_template = self._cals.get_schedule(self._sched_name, self.physical_qubits,
                                                  assign_params)
