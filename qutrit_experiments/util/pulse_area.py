dc8d5b9cf2ec53432aa68d9023f01a2216ddb67c
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-02-13 14:05:52 +0900

    minor tweak; renamed parameter

--- qutrit_experiments/util/pulse_area.py
+++ qutrit_experiments/util/pulse_area.py
@@ -5,13 +5,13 @@ from qiskit.providers import Backend
 from qiskit_experiments.framework import BackendData
 
 
-def grounded_gauss_area(sigma: float, n: float, gs_factor: bool = False) -> float:
+def grounded_gauss_area(sigma: float, rsr: float, gs_factor: bool = False) -> float:
     """Area of a truncated Gaussian with ends grounded to zero and peak normalized to unity."""
-    gauss_area = np.sqrt(2. * np.pi) * sigma * scispc.erf(n / np.sqrt(2.))
+    gauss_area = np.sqrt(2. * np.pi) * sigma * scispc.erf(rsr / np.sqrt(2.))
     # +1/sigma follows the Qiskit definition
-    pedestal = np.exp(-0.5 * ((n + 1. / sigma) ** 2))
+    pedestal = np.exp(-0.5 * ((rsr + 1. / sigma) ** 2))
 
-    area = (gauss_area - 2. * n * sigma * pedestal) / (1. - pedestal)
+    area = (gauss_area - 2. * rsr * sigma * pedestal) / (1. - pedestal)
 
     if gs_factor:
         # Empirical factor 1.16 to account for an undocumented difference between
