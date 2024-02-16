63278e61000cc4212c402038d43ee221dbf89841
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-02-16 16:50:00 +0900

    Commenting out platform CPU

--- qutrit_experiments/__init__.py
+++ qutrit_experiments/__init__.py
@@ -1,3 +1,4 @@
 import jax
 jax.config.update('jax_enable_x64', True)
-jax.config.update('jax_platform_name', 'cpu')
+# Uncomment if GPU is causing troubles
+#jax.config.update('jax_platform_name', 'cpu')
