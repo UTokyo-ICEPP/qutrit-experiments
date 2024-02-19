98d84c979cb42e71e7db221f90ad11d2d02fb28f
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-02-19 09:37:20 +0900

    Executable for qutrit_qubit_cx

---
+++ scripts/qutrit_qubit_cx.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    import os
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    import sys
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    import logging
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger('qutrit_experiments').setLevel(logging.INFO)
+
+    from qutrit_experiments.calibrations import (make_single_qutrit_gate_calibrations,
+                                                 make_qutrit_qubit_cx_calibrations)
+    from qutrit_experiments.programs.common import (get_program_config, load_calibrations,
+                                                    setup_backend, setup_data_dir, setup_runner)
+    from qutrit_experiments.programs.qutrit_qubit_cx import calibrate_qutrit_qubit_cx
+
+
+    program_config = get_program_config()
+    assert program_config['qubits'] is not None and len(program_config['qubits']) == 3
+    print('Starting qutrit_qubit_cx:', program_config['name'])
+    setup_data_dir(program_config)
+    backend = setup_backend(program_config)
+    calibrations = make_single_qutrit_gate_calibrations(backend)
+    calibrations = make_qutrit_qubit_cx_calibrations(backend, calibrations)
+    runner = setup_runner(backend, calibrations, program_config)
+    calibrated = load_calibrations(runner, program_config)
+
+    calibrate_qutrit_qubit_cx(runner, calibrated)
