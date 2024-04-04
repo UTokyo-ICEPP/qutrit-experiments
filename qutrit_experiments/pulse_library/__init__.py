c660dc7bf11c66d8db3e5177bdc548b7baf80842
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-04-04 17:15:25 +0900

    Forgot to include double drag in init

--- qutrit_experiments/pulse_library/__init__.py
+++ qutrit_experiments/pulse_library/__init__.py
@@ -1,4 +1,5 @@
 """Custom symbolic pulses."""
 
+from .double_drag import DoubleDrag, double_drag
 from .modulated_drag import ModulatedDrag, modulated_drag
 from .modulated_gaussiansquare import ModulatedGaussianSquare, modulated_gaussiansquare
