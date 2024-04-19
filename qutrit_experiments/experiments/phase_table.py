449480c80cd036727b4333e1e565e4d34d173965
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-04-19 17:58:11 +0900

    phase_table

---
+++ qutrit_experiments/experiments/phase_table.py
@@ -0,0 +1,62 @@
+from collections.abc import Sequence
+from typing import Optional
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.providers import Backend
+from qiskit.providers.options import Options
+from qiskit_experiments.framework import (AnalysisResultData, BaseAnalysis, BaseExperiment,
+                                          ExperimentData)
+from qiskit_experiments.framework.matplotlib import get_non_gui_ax
+
+from ..experiment_mixins import MapToPhysicalQubits
+
+
+class PhaseTable(MapToPhysicalQubits, BaseExperiment):
+    """Measure the phase of diagonals of a unitary."""
+    @classmethod
+    def _default_experiment_options(cls) -> Options:
+        options = super()._default_experiment_options()
+        options.circuit = None
+        options.angles = np.linspace(0., 2. * np.pi, 12, endpoint=False)
+        return options
+
+    def __init__(
+        self,
+        physical_qubits: Sequence[int],
+        circuit: QuantumCircuit,
+        backend: Optional[Backend] = None
+    ):
+        super().__init__(physical_qubits, backend=backend)
+        self.set_experiment_options(circuit=circuit)
+        self.analysis = None
+
+    def circuits(self) -> list[QuantumCircuit]:
+        circuits = []
+        num_qubits = len(self._physical_qubits)
+        phi = Parameter('phi')
+        for qubit in range(num_qubits):
+            for init in range(2 ** (num_qubits - 1)):
+                bits = [iq for iq in range(num_qubits) if iq != qubit and (init >> iq) % 2 == 1]
+                template = QuantumCircuit(num_qubits)
+                if bits:
+                    template.x(bits)
+                template.sx(qubit)
+                template.barrier()
+                template.compose(self.experiment_options.circuit, inplace=True)
+                template.barrier()
+                template.rz(phi, qubit)
+                template.sx(qubit)
+                template.measure_all()
+                template.metadata = {
+                    'measured_qubit': qubit,
+                    'init': init,
+                }
+
+                for angle in self.experiment_options.angles:
+                    circuits.append(template.assign_parameters({phi: angle}, inplace=False))
+
+        return circuits
+
