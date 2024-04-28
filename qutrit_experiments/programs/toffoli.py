1f29c429588e453453ed235e3eb8b71b88804d0d
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-04-28 14:57:32 +0900

    adding a new function quickly - a cooler solution exists

--- qutrit_experiments/programs/toffoli.py
+++ qutrit_experiments/programs/toffoli.py
@@ -12,6 +12,20 @@ def calibrate_toffoli(
     runner.run_experiment('c1c2_rotary_delta')
 
 
+def characterize_ccz(
+    runner: ExperimentsRunner,
+    refresh_readout_error: bool = True
+):
+    if 'readout_assignment_matrices' not in runner.program_data:
+        # Construct the error mitigation matrix and find the rough CR pulse width
+        runner.run_experiment('qubits_assignment_error', force_resubmit=refresh_readout_error)
+
+    # Truth table
+    runner.run_experiment('ccz_truth_table')
+    runner.run_experiment('ccz_phase_table')
+    runner.run_experiment('ccz_qpt_bc')
+
+
 def characterize_toffoli(
     runner: ExperimentsRunner,
     refresh_readout_error: bool = True
