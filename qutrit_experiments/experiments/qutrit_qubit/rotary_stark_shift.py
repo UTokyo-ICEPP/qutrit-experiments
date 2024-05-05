12d639d579bf5d615c938dbcb2d590dd9087e9fa
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-05-05 23:56:56 +0900

    And rotary Stark

--- qutrit_experiments/experiments/qutrit_qubit/rotary_stark_shift.py
+++ qutrit_experiments/experiments/qutrit_qubit/rotary_stark_shift.py
@@ -7,16 +7,17 @@ from qiskit.pulse import Schedule, ScheduleBlock
 from qiskit.circuit import Gate
 from qiskit.providers import Backend
 from qiskit_experiments.framework import Options, ExperimentData
-from qiskit_experiments.calibration_management import Calibrations
+from qiskit_experiments.calibration_management import BaseCalibrationExperiment, Calibrations
 from qiskit_experiments.calibration_management.update_library import BaseUpdater
 
 from ...gates import X12Gate, SX12Gate
-from ..single_qutrit.stark_shift_phase import BasePhaseRotation, UpdateStarkDelta
+from ...framework.calibration_updaters import DeltaUpdater
+from ..phase_shift import PhaseShiftMeasurement
 
 twopi = 2. * np.pi
 
 
-class RotaryQutritPhaseRotation(BasePhaseRotation):
+class RotaryQutritPhaseRotation(PhaseShiftMeasurement):
     r"""Experiment to measure the phase shift on :math:`|2\rangle` by the rotary tone of
     qubit-qutrit CR.
 
@@ -79,28 +80,32 @@ class RotaryQutritPhaseRotation(BasePhaseRotation):
         return template
 
 
-class RotaryStarkShiftPhaseCal(UpdateStarkDelta, RotaryQutritPhaseRotation):
-    """Calibration experiment for RotaryQutritPhaseRotation."""
-    _cal_parameter_name = 'delta_rzx45p_rotary'
+class RotaryStarkShiftUpdater(DeltaUpdater):
+    @staticmethod
+    def get_value(exp_data: ExperimentData, param_name: str, index: Optional[int] = -1) -> float:
+        delta = -1. * BaseUpdater.get_value(exp_data, 'phase_offset', index)
+        return (delta + np.pi) % twopi - np.pi
 
+
+class RotaryStarkShiftPhaseCal(BaseCalibrationExperiment, RotaryQutritPhaseRotation):
+    """Calibration experiment for RotaryQutritPhaseRotation."""
     def __init__(
         self,
         physical_qubits: Sequence[int],
         calibrations: Calibrations,
         phase_shifts: Optional[Sequence[float]] = None,
         backend: Optional[Backend] = None,
-        cal_parameter_name: Optional[str] = None,
-        schedule_name: Optional[str] = None,
         auto_update: bool = True
     ):
         super().__init__(
-            physical_qubits,
             calibrations,
+            physical_qubits,
+            schedule_name=None,
+            cal_parameter_name='delta_rzx45p_rotary',
+            updater=RotaryStarkShiftUpdater,
+            auto_update=auto_update,
             phase_shifts=phase_shifts,
-            backend=backend,
-            cal_parameter_name=cal_parameter_name,
-            schedule_name=schedule_name,
-            auto_update=auto_update
+            backend=backend
         )
 
         qubits = tuple(physical_qubits)
@@ -115,6 +120,5 @@ class RotaryStarkShiftPhaseCal(UpdateStarkDelta, RotaryQutritPhaseRotation):
 
         self.set_experiment_options(schedule=sched)
 
-    def _extract_delta(self, experiment_data: ExperimentData) -> float:
-        """See the docstring of RotaryQutritPhaseRotation."""
-        return -BaseUpdater.get_value(experiment_data, 'phase_offset')
+    def _attach_calibrations(self, circuit: QuantumCircuit):
+        pass
