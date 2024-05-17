abb2868bddf4797f149f039ad2b9bee52032c893
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-05-18 01:53:44 +0900

    Configurations and script for Toffoli paper data taking

---
+++ qutrit_experiments/configurations/qutrit_toffoli_paper.py
@@ -0,0 +1,502 @@
+"""Experiments for taking data shown in the qutrit Toffoli paper."""
+import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.circuit.equivalence_library import SessionEquivalenceLibrary as sel
+from qiskit.transpiler import PassManager, StagedPassManager
+from qiskit.transpiler.passes import BasisTranslator
+from qiskit_experiments.calibration_management import ParameterValue
+from qiskit_experiments.exceptions import CalibrationError
+
+from qutrit_experiments.configurations.common import add_readout_mitigation, add_qpt_readout_mitigation
+from qutrit_experiments.experiment_config import ExperimentConfig, register_exp
+from qutrit_experiments.gates import P2Gate, QutritCCZGate, X12Gate, XplusGate, XminusGate
+from qutrit_experiments.transpilation.qutrit_transpiler import (BASIS_GATES,
+                                                                transpile_qutrit_circuits, make_instruction_durations)
+from qutrit_experiments.transpilation.layout_and_translation import UndoLayout, generate_layout_passmanager
+from qutrit_experiments.transpilation.dynamical_decoupling import DDCalculator
+from qutrit_experiments.util.qutrit_toffoli import qutrit_toffoli_circuit
+
+###########################
+### Circuit definitions ###
+###########################
+
+def total_ccz_duration_nobc(runner):
+    ecr_dur_c1c2 = runner.backend.target['ecr'][runner.qubits[:2]].calibration.duration
+    ecr_dur_tc2 = runner.backend.target['ecr'][runner.qubits[2:0:-1]].calibration.duration
+    x_dur = runner.backend.target['x'][runner.qubits[1:2]].calibration.duration
+
+    alpha = x_dur + ecr_dur_tc2 + x_dur + ecr_dur_tc2
+    duration = 2 * x_dur
+    duration += ecr_dur_c1c2 + x_dur
+    duration += alpha
+    duration += 2 * x_dur + alpha
+    duration += ecr_dur_c1c2
+    duration += 2 * x_dur
+    return duration
+
+def total_ccz_duration_bc(runner):
+    ecr_dur_c1c2 = runner.backend.target['ecr'][runner.qubits[:2]].calibration.duration
+    ecr_dur_tc2 = runner.backend.target['ecr'][runner.qubits[2:0:-1]].calibration.duration
+    x_dur = runner.backend.target['x'][runner.qubits[1:2]].calibration.duration
+
+    alpha = x_dur + ecr_dur_tc2 + x_dur + ecr_dur_tc2
+    duration = 2 * x_dur
+    duration += ecr_dur_c1c2
+    duration += alpha
+    duration += 2 * x_dur + alpha
+    duration += ecr_dur_c1c2
+    t2 = duration
+    duration += 2 * x_dur
+    ref_delay = t2 - 3 * alpha - 2 * x_dur
+    if ref_delay >= 0:
+        duration += ref_delay + 2 * x_dur
+    else:
+        raise NotImplementedError('Basis-cycled CCZ needs negative delay')
+
+    return duration
+
+def xminusxplus_circuit(runner):
+    x_dur = runner.backend.target['x'][runner.qubits[1:2]].calibration.duration
+
+    circuit = QuantumCircuit(1)
+    circuit.x(0)
+    circuit.append(X12Gate(), [0])
+    circuit.delay(total_ccz_duration_nobc(runner) - 4 * x_dur, 0)
+    circuit.append(X12Gate(), [0])
+    circuit.x(0)
+
+    return circuit
+
+def xplus3_circuit(runner):
+    x_dur = runner.backend.target['x'][runner.qubits[1:2]].calibration.duration
+    delay = (total_ccz_duration_bc(runner) - 6 * x_dur) // 2
+
+    circuit = QuantumCircuit(1)
+    circuit.append(X12Gate(), [0])
+    circuit.x(0)
+    circuit.delay(delay, 0)
+    circuit.append(X12Gate(), [0])
+    circuit.x(0)
+    circuit.delay(delay, 0)
+    circuit.append(X12Gate(), [0])
+    circuit.x(0)
+
+    return circuit
+
+def skeleton_cz_circuit_core(runner, ecr_tc2=False):
+    """Skeleton CZ-like circuit."""
+    instruction_durations = make_instruction_durations(runner.backend, runner.calibrations,
+                                                       runner.qubits)
+    def dur(gate, *qubits):
+        return instruction_durations.get(gate, tuple(runner.qubits[iq] for iq in qubits))
+
+    pulse_alignment = runner.backend.target.pulse_alignment
+    ddapp = DDCalculator(runner.qubits, instruction_durations, pulse_alignment)
+
+    x_dur = dur('x', 1)
+    xplus_dur = dur('x12', 1) + x_dur
+    ecr_dur_tc2 = dur('ecr', 2, 1)
+
+    alpha = x_dur + ecr_dur_tc2 + x_dur + ecr_dur_tc2
+
+    circuit = QuantumCircuit(3)
+
+    # Reverse CX (missing the last X of c2 and a block of duration 2X on t)
+    ddapp.append_dd(circuit, 0, 2 * alpha + xplus_dur, 5)
+    circuit.append(X12Gate(), [1])
+    if ecr_tc2:
+        circuit.ecr(2, 1)
+        circuit.delay(x_dur, 1)
+        circuit.x(2)
+        circuit.ecr(2, 1)
+        circuit.x(2)
+        circuit.append(P2Gate(-np.pi / 2.), [0])
+        circuit.rz(-np.pi, 1)
+    else:
+        circuit.x(1)
+        circuit.delay(alpha - xplus_dur, 1)
+        ddapp.append_dd(circuit, 2, alpha, 2, distribution='right')
+
+    circuit.append(X12Gate(), [1])
+    circuit.x(1)
+    circuit.delay(alpha - xplus_dur, 1)
+    circuit.append(X12Gate(), [1])
+    circuit.delay(x_dur, 2)
+    ddapp.append_dd(circuit, 2, alpha - xplus_dur, distribution='left')
+
+    return circuit
+
+def skeleton_cz_circuit(runner, delta_cz=0., ecr_tc2=False):
+    instruction_durations = make_instruction_durations(runner.backend, runner.calibrations,
+                                                       runner.qubits)
+    x_dur = instruction_durations.get('x', runner.qubits[2:])
+
+    circuit = QuantumCircuit(3)
+
+    if delta_cz:
+        circuit.rz(-delta_cz, 1)
+
+    circuit.compose(skeleton_cz_circuit_core(runner, ecr_tc2=ecr_tc2), inplace=True)
+    circuit.x(1)
+    circuit.delay(2 * x_dur, 2)
+
+    return circuit
+
+def skeleton_ccz_circuit(runner, delta_cz=0., delta_ccz=0., ecr_c1c2=False, ecr_tc2=False):
+    """Skeleton CCZ-like circuit."""
+    instruction_durations = make_instruction_durations(runner.backend, runner.calibrations,
+                                                       runner.qubits)
+    def dur(gate, *qubits):
+        return instruction_durations.get(gate, tuple(runner.qubits[iq] for iq in qubits))
+
+    pulse_alignment = runner.backend.target.pulse_alignment
+    ddapp = DDCalculator(runner.qubits, instruction_durations, pulse_alignment)
+
+    x_dur = dur('x', 1)
+    x12_dur = dur('x12', 1)
+    xplus_dur = dur('x12', 1) + x_dur
+    ecr_dur_c1c2 = dur('ecr', 0, 1)
+    cr_dur_c1c2 = (ecr_dur_c1c2 - x_dur) // 2
+    ecr_dur_tc2 = dur('ecr', 2, 1)
+
+    alpha = x_dur + ecr_dur_tc2 + x_dur + ecr_dur_tc2
+    t2 = 2 * x_dur
+    t2 += ecr_dur_c1c2
+    t2 += alpha
+    t2 += 2 * x_dur + alpha
+    t2 += ecr_dur_c1c2
+    refocusing_delay = t2 - 3 * alpha - 2 * x_dur
+    if refocusing_delay < 0:
+        raise NotImplementedError('Negative refocusing delay')
+
+    circuit = QuantumCircuit(3)
+    # X+X+ (last X in the next block)
+    ddapp.append_dd(circuit, 0, 2 * xplus_dur + refocusing_delay, distribution='left')
+    circuit.append(X12Gate(), [1])
+    circuit.x(1)
+    circuit.delay(refocusing_delay, 1)
+    circuit.append(X12Gate(), [1])
+    ddapp.append_dd(circuit, 2, 2 * xplus_dur + refocusing_delay, distribution='right')
+
+    # ECR
+    if ecr_c1c2:
+        circuit.sx(1)
+        circuit.append(P2Gate(-np.pi / 4.), [1])
+        circuit.ecr(0, 1)
+    else:
+        circuit.x(1)
+        circuit.delay(cr_dur_c1c2, 0)
+        circuit.x(0)
+        circuit.delay(cr_dur_c1c2, 0)
+        circuit.delay(ecr_dur_c1c2, 1)
+    ddapp.append_dd(circuit, 2, ecr_dur_c1c2 + x12_dur, 2)
+
+    if delta_cz:
+        circuit.rz(-delta_cz, 1)
+
+    # Reverse CX (last X in the next block)
+    circuit.compose(skeleton_cz_circuit_core(runner, ecr_tc2=ecr_tc2), inplace=True)
+    circuit.x(2)
+    circuit.delay(x_dur, 2)
+
+    # ECR
+    if ecr_c1c2:
+        circuit.rz(np.pi, 1)
+        circuit.sx(1)
+        circuit.rz(-np.pi, 1)
+        circuit.append(P2Gate(3. * np.pi / 4.), [1])
+        circuit.ecr(0, 1)
+    else:
+        circuit.x(1)
+        circuit.delay(cr_dur_c1c2, 0)
+        circuit.x(0)
+        circuit.delay(cr_dur_c1c2, 0)
+        circuit.delay(ecr_dur_c1c2, 1)
+
+    circuit.delay(xplus_dur, 0)
+    circuit.append(X12Gate(), [1])
+    circuit.x(1)
+    ddapp.append_dd(circuit, 2, ecr_dur_c1c2 + x_dur, 2)
+    circuit.x(2)
+
+    if delta_ccz:
+        circuit.rz(-delta_ccz, 1)
+
+    return circuit
+
+def translate(circuit, runner):
+    basis_gates = runner.backend.basis_gates + [g.gate_name for g in BASIS_GATES]
+    pms = {
+        'layout': generate_layout_passmanager(runner.qubits, runner.backend.coupling_map),
+        'translation': PassManager([BasisTranslator(sel, basis_gates)]),
+        'layin': PassManager([UndoLayout(runner.qubits)])
+    }
+    return StagedPassManager(list(pms.keys()), **pms).run(circuit)
+
+def id0_circuit(runner):
+    """Circuit with only the Xpluses and DDs."""
+    delta_cz = runner.calibrations.get_parameter_value('delta_cz_id', runner.qubits)
+    delta_ccz = runner.calibrations.get_parameter_value('delta_ccz_id0', runner.qubits)
+    circuit = skeleton_ccz_circuit(runner, delta_cz=delta_cz, delta_ccz=delta_ccz)
+    return translate(circuit, runner)
+
+def id1_circuit(runner):
+    """Circuit with Xpluses, DDs, and ECR(c1,c2)."""
+    delta_cz = runner.calibrations.get_parameter_value('delta_cz_id', runner.qubits)
+    delta_ccz = runner.calibrations.get_parameter_value('delta_ccz_id1', runner.qubits)
+    circuit = skeleton_ccz_circuit(runner, delta_cz=delta_cz, delta_ccz=delta_ccz, ecr_c1c2=True)
+    return translate(circuit, runner)
+
+def cz_circuit(runner):
+    """Circuit with Xpluses, DDs, and ECR(c1,c2)."""
+    delta_cz = runner.calibrations.get_parameter_value('delta_cz', runner.qubits)
+    delta_ccz = runner.calibrations.get_parameter_value('delta_ccz_cz', runner.qubits)
+    circuit = skeleton_ccz_circuit(runner, delta_cz=delta_cz, delta_ccz=delta_ccz, ecr_c2t=True)
+    return translate(circuit, runner)
+
+#######################################
+### Calibrations for error analysis ###
+#######################################
+
+@register_exp
+@add_readout_mitigation
+def cz_id_c2_phase(runner):
+    from ..experiments.phase_table import DiagonalPhaseCal
+
+    param_name = 'delta_cz_id'
+    try:
+        delta_cz = runner.calibrations.get_parameter_value(param_name, runner.qubits)
+    except CalibrationError:
+        runner.calibrations.add_parameter_value(ParameterValue(0.), param_name, runner.qubits)
+        delta_cz = 0.
+    circuit = skeleton_cz_circuit(runner, delta_cz=delta_cz)
+
+    return ExperimentConfig(
+        DiagonalPhaseCal,
+        runner.qubits,
+        args={
+            'circuit': circuit,
+            'state': [0, None, 0],
+            'cal_parameter_name': param_name
+        },
+        analysis_options={'outcome': '010'}
+    )
+
+@register_exp
+@add_readout_mitigation
+def ccz_id0_c2_phase(runner):
+    from ..experiments.phase_table import DiagonalPhaseCal
+
+    param_name = 'delta_ccz_id0'
+    delta_cz = runner.calibrations.get_parameter_value('delta_cz_id', runner.qubits)
+    try:
+        delta_ccz = runner.calibrations.get_parameter_value(param_name, runner.qubits)
+    except CalibrationError:
+        delta_ccz = runner.calibrations.add_parameter_value(ParameterValue(0.), param_name,
+                                                            runner.qubits)
+
+    circuit = skeleton_ccz_circuit(runner, delta_cz=delta_cz, delta_ccz=delta_ccz)
+
+    return ExperimentConfig(
+        DiagonalPhaseCal,
+        runner.qubits,
+        args={
+            'circuit': circuit,
+            'state': [0, None, 0],
+            'cal_parameter_name': param_name
+        },
+        analysis_options={'outcome': '010'}
+    )
+
+@register_exp
+@add_readout_mitigation
+def ccz_id1_c2_phase(runner):
+    from ..experiments.phase_table import DiagonalPhaseCal
+
+    param_name = 'delta_ccz_id1'
+    delta_cz = runner.calibrations.get_parameter_value('delta_cz_id', runner.qubits)
+    try:
+        delta_ccz = runner.calibrations.get_parameter_value(param_name, runner.qubits)
+    except CalibrationError:
+        delta_ccz = runner.calibrations.add_parameter_value(ParameterValue(0.), param_name,
+                                                            runner.qubits)
+
+    circuit = skeleton_ccz_circuit(runner, delta_cz=delta_cz, delta_ccz=delta_ccz, ecr_c1c2=True)
+
+    return ExperimentConfig(
+        DiagonalPhaseCal,
+        runner.qubits,
+        args={
+            'circuit': circuit,
+            'state': [0, None, 0],
+            'cal_parameter_name': param_name
+        },
+        analysis_options={'outcome': '010'}
+    )
+
+@register_exp
+@add_readout_mitigation
+def ccz_cz_c2_phase(runner):
+    from ..experiments.phase_table import DiagonalPhaseCal
+
+    param_name = 'delta_ccz_cz'
+    delta_cz = runner.calibrations.get_parameter_value('delta_cz', runner.qubits)
+    try:
+        delta_ccz = runner.calibrations.get_parameter_value(param_name, runner.qubits)
+    except CalibrationError:
+        delta_ccz = runner.calibrations.add_parameter_value(ParameterValue(0.), param_name,
+                                                            runner.qubits)
+
+    circuit = skeleton_ccz_circuit(runner, delta_cz=delta_cz, delta_ccz=delta_ccz, ecr_tc2=True)
+
+    return ExperimentConfig(
+        DiagonalPhaseCal,
+        runner.qubits,
+        args={
+            'circuit': circuit,
+            'state': [0, None, 0],
+            'cal_parameter_name': param_name
+        },
+        analysis_options={'outcome': '010'}
+    )
+
+
+#################################
+### Basis-cycle demonstrators ###
+#################################
+
+@register_exp
+@add_readout_mitigation
+def c2phase_xminusxplus(runner):
+    from qutrit_experiments.experiments.phase_table import DiagonalCircuitPhaseShift
+
+    return ExperimentConfig(
+        DiagonalCircuitPhaseShift,
+        runner.qubits[1:2],
+        args={
+            'circuit': xminusxplus_circuit(runner),
+            'state': [None],
+        }
+    )
+
+@register_exp
+@add_qpt_readout_mitigation
+def qpt_xminusxplus(runner):
+    from qutrit_experiments.experiments.process_tomography import CircuitTomography
+
+    return ExperimentConfig(
+        CircuitTomography,
+        runner.qubits[1:2],
+        args={
+            'circuit': xminusxplus_circuit(runner),
+            'target_circuit': QuantumCircuit(1)
+        },
+        run_options={'shots': 2000},
+        analysis_options={'target_bootstrap_samples': 100}
+    )
+
+@register_exp
+@add_readout_mitigation
+def c2phase_xplus3(runner):
+    from qutrit_experiments.experiments.phase_table import DiagonalCircuitPhaseShift
+
+    return ExperimentConfig(
+        DiagonalCircuitPhaseShift,
+        runner.qubits[1:2],
+        args={
+            'circuit': xplus3_circuit(runner),
+            'state': [None],
+        }
+    )
+
+@register_exp
+@add_qpt_readout_mitigation
+def qpt_xplus3(runner):
+    from qutrit_experiments.experiments.process_tomography import CircuitTomography
+
+    return ExperimentConfig(
+        CircuitTomography,
+        runner.qubits[1:2],
+        args={
+            'circuit': xplus3_circuit(runner),
+            'target_circuit': QuantumCircuit(1)
+        },
+        run_options={'shots': 2000},
+        analysis_options={'target_bootstrap_samples': 100}
+    )
+
+######################
+### Error analysis ###
+######################
+
+@register_exp
+@add_readout_mitigation(probability=False)
+def truthtable_id0(runner):
+    from qutrit_experiments.experiments.truth_table import TruthTable
+    return ExperimentConfig(
+        TruthTable,
+        runner.qubits,
+        args={
+            'circuit': id0_circuit(runner)
+        }
+    )
+
+@register_exp
+@add_readout_mitigation
+def phasetable_id0(runner):
+    from qutrit_experiments.experiments.phase_table import PhaseTable
+    return ExperimentConfig(
+        PhaseTable,
+        runner.qubits,
+        args={
+            'circuit': id0_circuit(runner)
+        }
+    )
+
+@register_exp
+@add_readout_mitigation(probability=False)
+def truthtable_id1(runner):
+    from qutrit_experiments.experiments.truth_table import TruthTable
+    return ExperimentConfig(
+        TruthTable,
+        runner.qubits,
+        args={
+            'circuit': id1_circuit(runner)
+        }
+    )
+
+@register_exp
+@add_readout_mitigation
+def phasetable_id1(runner):
+    from qutrit_experiments.experiments.phase_table import PhaseTable
+    return ExperimentConfig(
+        PhaseTable,
+        runner.qubits,
+        args={
+            'circuit': id1_circuit(runner)
+        }
+    )
+
+@register_exp
+@add_readout_mitigation(probability=False)
+def truthtable_cz(runner):
+    from qutrit_experiments.experiments.truth_table import TruthTable
+    return ExperimentConfig(
+        TruthTable,
+        runner.qubits,
+        args={
+            'circuit': cz_circuit(runner)
+        }
+    )
+
+@register_exp
+@add_readout_mitigation
+def phasetable_cz(runner):
+    from qutrit_experiments.experiments.phase_table import PhaseTable
+    return ExperimentConfig(
+        PhaseTable,
+        runner.qubits,
+        args={
+            'circuit': cz_circuit(runner)
+        }
+    )
---
+++ scripts/qutrit_toffoli_paper_ccz.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+"""Experiments characterizing the CCZ sequence for the qutrit Toffoli paper."""
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import logging
+
+if __name__ == '__main__':
+    from qutrit_experiments.programs.program_config import get_program_config
+    program_config = get_program_config()
+
+    try:
+        import gpustat
+    except ImportError:
+        gpu_id = 0
+    else:
+        gpu_id = max(gpustat.new_query(), key=lambda g: g.memory_free).index
+    os.environ['CUDA_VISIBLE_DEVICES'] = f'{gpu_id}'
+    import jax
+    jax.config.update('jax_enable_x64', True)
+    import jax.numpy as jnp
+    jnp.zeros(1)
+
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger('qutrit_experiments').setLevel(logging.INFO)
+
+    from qiskit.circuit import Parameter
+    from qutrit_experiments.calibrations import (make_single_qutrit_gate_calibrations,
+                                                 make_qutrit_qubit_cx_calibrations,
+                                                 make_toffoli_calibrations)
+    from qutrit_experiments.configurations.common import (qubits_assignment_error as _assign_error,
+                                                          qubits_assignment_error_post as _assign_post)
+    import qutrit_experiments.configurations.full_backend_qutrits
+    import qutrit_experiments.configurations.qutrit_qubit_cx
+    import qutrit_experiments.configurations.toffoli
+    import qutrit_experiments.configurations.qutrit_toffoli_paper
+    from qutrit_experiments.experiment_config import (BatchExperimentConfig,
+                                                      ParallelExperimentConfig,
+                                                      experiments, postexperiments)
+    from qutrit_experiments.gates import QutritQubitCXType
+    from qutrit_experiments.programs.common import (load_calibrations, setup_backend,
+                                                    setup_data_dir, setup_runner)
+    from qutrit_experiments.programs.common import run_experiment
+    from qutrit_experiments.programs.single_qutrit_gates import calibrate_single_qutrit_gates
+    from qutrit_experiments.runners.parallel_runner import ParallelRunner
+
+
+    # Create the data directory
+    setup_data_dir(program_config)
+    assert program_config['qubits'] is not None and len(program_config['qubits']) % 3 == 0
+    print('Starting toffoli:', program_config['name'])
+
+    # Instantiate the backend
+    backend = setup_backend(program_config)
+    all_qubits = tuple(program_config['qubits'])
+    qubits_list = [all_qubits[i:i + 3] for i in range(0, len(all_qubits), 3)]
+    qutrits = [qubits[1] for qubits in qubits_list]
+
+    # Define all schedules to be calibrated
+    calibrations = make_single_qutrit_gate_calibrations(backend, qubits=qutrits)
+    make_qutrit_qubit_cx_calibrations(backend, calibrations=calibrations, qubits=all_qubits)
+    make_toffoli_calibrations(backend, calibrations=calibrations, qubits=all_qubits)
+    calibrations._register_parameter(Parameter('delta_cz_id'), ())
+    calibrations._register_parameter(Parameter('delta_ccz_id0'), ())
+    calibrations._register_parameter(Parameter('delta_ccz_id1'), ())
+    calibrations._register_parameter(Parameter('delta_ccz_cz'), ())
+
+    for qubits in qubits_list:
+        calibrations.add_parameter_value(int(QutritQubitCXType.REVERSE), 'rcr_type', qubits[1:])
+
+    # Define the main ExperimentsRunner and run the readout mitigation measurements
+    runner = setup_runner(backend, program_config, calibrations=calibrations)
+    runner.job_retry_interval = 120
+    runner.default_print_level = 1
+
+    def parallelized(exp_type, gen=None, qidx=None, **options):
+        config = ParallelExperimentConfig(exp_type=exp_type, **options)
+        for qubits in qubits_list:
+            if qidx:
+                runner.qubits = [qubits[iq] for iq in qidx]
+            else:
+                runner.qubits = qubits
+            if gen:
+                subconfig = gen(runner)
+            else:
+                subconfig = experiments[exp_type](runner)
+            subconfig.exp_type = f'{exp_type}-{"_".join(map(str, runner.qubits))}'
+            config.subexperiments.append(subconfig)
+
+        return config
+
+    try:
+        config = parallelized('qubits_assignment_error',
+                              gen=lambda runner: _assign_error(runner, runner.qubits),
+                              run_options={'shots': 10000})
+        postexperiments.pop('qubits_assignment_error')
+        data = run_experiment(runner, config, plot_depth=-1,
+                              force_resubmit=program_config['refresh_readout'])
+        for qubits, child_data in zip(qubits_list, data.child_data()):
+            runner.qubits = qubits
+            _assign_post(runner, child_data)
+
+        # Load the calibrations if source is specified in program_config
+        calibrated = load_calibrations(runner, program_config)
+
+        # Define a ParallelRunner to calibrate single qutrit gates in parallel
+        qutrit_runner = setup_runner(backend, program_config, calibrations=calibrations,
+                                     qubits=qutrits, runner_cls=ParallelRunner)
+        qutrit_runner.program_data = runner.program_data
+        qutrit_runner.runtime_session = runner.runtime_session
+        qutrit_runner.job_retry_interval = 120
+        qutrit_runner.default_print_level = 0
+
+        # Single qutrit gates
+        calibrate_single_qutrit_gates(qutrit_runner,
+                                      refresh_readout_error=program_config['refresh_readout'],
+                                      calibrated=calibrated)
+        # Session may have been renewed
+        runner.runtime_session = qutrit_runner.runtime_session
+
+        if (exp_type := 'tc2_cr_rotary_delta') not in calibrated:
+            config = parallelized(exp_type, qidx=(1, 2))
+            run_experiment(runner, config, plot_depth=-1)
+
+        if (exp_type := 'c1c2_cr_rotary_delta') not in calibrated:
+            config = parallelized(exp_type)
+            run_experiment(runner, config, plot_depth=-1)
+
+        batch_config = BatchExperimentConfig(exp_type='delta_cz_cal')
+        for exp_type in ['cz_c2_phase', 'cz_id_c2_phase']:
+            if exp_type not in calibrated:
+                config = parallelized(exp_type)
+                batch_config.subexperiments.append(config)
+        if batch_config.subexperiments:
+            run_experiment(runner, batch_config, plot_depth=-1)
+
+        batch_config = BatchExperimentConfig(exp_type='delta_ccz_cal')
+        for exp_type in [
+            'ccz_c2_phase',
+            'ccz_id0_c2_phase',
+            'ccz_id1_c2_phase',
+            'ccz_cz_c2_phase'
+        ]:
+            if exp_type not in calibrated:
+                config = parallelized(exp_type)
+                batch_config.subexperiments.append(config)
+        if batch_config.subexperiments:
+            run_experiment(runner, batch_config, plot_depth=-1)
+
+        config = parallelized(
+            exp_type='qpt_ccz_bc',
+            experiment_options={'max_circuits': 100},
+            run_options={'shots': 2000},
+            analysis_options={'parallelize': 0}
+        )
+        data_qpt = run_experiment(runner, config, block_for_results=False, plot_depth=-1)
+
+        batch_config = BatchExperimentConfig(
+            exp_type='characterization_3q',
+            experiment_options={'max_circuits': 100},
+            run_options={'shots': 2000},
+        )
+        for exp_type in [
+            'truthtable_ccz',
+            'phasetable_ccz',
+            'truthtable_id0',
+            'phasetable_id0',
+            'truthtable_id1',
+            'phasetable_id1',
+            'truthtable_cz',
+            'phasetable_cz'
+        ]:
+            config = parallelized(exp_type)
+            batch_config.subexperiments.append(config)
+        data_3q = run_experiment(runner, config, block_for_results=False, plot_depth=-1)
+
+        batch_config = BatchExperimentConfig(
+            exp_type='characterization_1q',
+            run_options={'shots': 2000}
+        )
+        for exp_type in [
+            'c2phase_xminusxplus',
+            'qpt_xminusxplus',
+            'c2phase_xplus3',
+            'qpt_xplus3'
+        ]:
+            config = parallelized(exp_type)
+            batch_config.subexperiments.append(config)
+        data_1q = run_experiment(runner, config, block_for_results=False, plot_depth=-1)
+
+    finally:
+        runner.runtime_session.close()
+
+    data_qpt.block_for_results()
+    data_3q.block_for_results()
+    data_1q.block_for_results()
+
+    runner.program_data['choi'] = {}
+    runner.program_data['process_fidelity'] = {}
+    for child_data in data_qpt.child_data():
+        qubits = tuple(child_data.metadata['physical_qubits'])
+        runner.program_data['choi'][qubits] = child_data.analysis_results('state').value
+        runner.program_data['process_fidelity'][qubits] = child_data.analysis_results('process_fidelity').value
+
+    for pdata in data_3q.child_data():
+        exp_type = pdata.experiment_type
+        runner.program_data[exp_type] = {}
+        for child_data in pdata.child_data():
+            qubits = tuple(child_data.metadata['physical_qubits'])
+            if exp_type.startswith('truthtable'):
+                runner.program_data[exp_type][qubits] = child_data.analysis_results('truth_table').value
+            else:
+                runner.program_data[exp_type][qubits] = child_data.analysis_results('phases').value
+
+    for pdata in data_1q.child_data():
+        exp_type = pdata.experiment_type
+        runner.program_data[exp_type] = {}
+        for child_data in pdata.child_data():
+            qubits = tuple(child_data.metadata['physical_qubits'])
+            if exp_type.startswith('c2phase'):
+                runner.program_data[exp_type][qubits] = child_data.analysis_results('phase_offset').value
+            else:
+                runner.program_data[exp_type][qubits] = child_data.analysis_results('process_fidelity').value
